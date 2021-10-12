054106cb2982b63f9108f7f5126ee838cd6226f7
ksc-terada <63327855+ksc-terada@users.noreply.github.com>
2021-10-12 17:08:13 +0900

    Add files via upload

---
+++ Labeling/Labeling/CsvParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Labeling
+{
+    class CsvParser
+    {
+        private static string FilePath = "";
+        public CsvParser(string path)
+        {
+            FilePath = path;
+        }
+
+        public IEnumerable<string[]> GetLine()
+        {
+            string separator = ",";
+            Encoding encoding = Encoding.GetEncoding("shift-jis");
+            using (Stream stream = new System.IO.FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (TextFieldParser parser = new TextFieldParser(stream, encoding ?? Encoding.UTF8, true, false))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.Delimiters = new[] { separator };
+                parser.HasFieldsEnclosedInQuotes = true;
+                parser.TrimWhiteSpace = true;
+                while (parser.EndOfData == false)
+                {
+                    string[] fields = parser.ReadFields();
+                    yield return fields;
+                }
+            }
+        }
+    }
+}
